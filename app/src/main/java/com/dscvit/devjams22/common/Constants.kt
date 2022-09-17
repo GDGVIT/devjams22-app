21fd08e2025966b6d872a7be26135450f47a9064
PrashannaR <prashanna.rajbhandari1@gmail.com>
2022-09-18 00:16:59 +0530

    feat: home screen ui update

--- app/src/main/java/com/dscvit/devjams22/common/Constants.kt
+++ app/src/main/java/com/dscvit/devjams22/common/Constants.kt
@@ -1,4 +1,5 @@
 package com.dscvit.devjams22.common
 
 object Constants {
+    const val discordLink = "https://dscv.it/dj22discord"
 }
--- app/src/main/java/com/dscvit/devjams22/presentation/home/components/Home.kt
+++ app/src/main/java/com/dscvit/devjams22/presentation/home/components/Home.kt
@@ -1,11 +1,15 @@
 package com.dscvit.devjams22.presentation.home.components
 
+import android.content.Intent
+import android.net.Uri
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -13,13 +17,24 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat.startActivity
 import com.dscvit.devjams22.R
+import com.dscvit.devjams22.common.Constants
+import com.dscvit.devjams22.presentation.ui.theme.GoogleGreen
+import com.dscvit.devjams22.presentation.ui.theme.GoogleRed
+import com.dscvit.devjams22.presentation.ui.theme.GoogleYellow
 import com.dscvit.devjams22.presentation.ui.theme.GreyBackground
+
 
 @Preview(showSystemUi = true)
 @Composable
@@ -83,23 +98,142 @@ fun Home() {
                         .clip(shape = RoundedCornerShape(10.dp))
                         .background(Color.White)
 
+
                 ) {
                     Image(
                         painter = painterResource(id = R.drawable.notification),
                         contentDescription = null,
-                        modifier = Modifier.align(Alignment.Center)
+                        modifier = Modifier
+                            .align(Alignment.Center)
                             .size(24.dp)
                     )
 
                 }
 
 
-        }
-
-
+            }
 
         }
+
+        val openDiscordUrl: Uri = Uri.parse(Constants.discordLink)
+        val intent = Intent(Intent.ACTION_VIEW, openDiscordUrl)
+        val context = LocalContext.current
+
+        Card(
+            backgroundColor = GreyBackground,
+            elevation = 0.dp,
+            modifier = Modifier.clickable {
+                startActivity(context, intent, null)
+            }
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.join_discord), contentDescription = null,
+                modifier = Modifier.padding(start = 16.dp)
+            )
+
+            Column(modifier = Modifier.padding(top = 50.dp, start = 45.dp)) {
+                Text(
+                    text = buildAnnotatedString {
+                        withStyle(
+                            style = SpanStyle(
+                                color = colorResource(id = R.color.JoinDiscordColor),
+                                fontSize = 20.sp
+                            )
+                        ) {
+                            append("Join the #")
+                        }
+                        withStyle(
+                            style = SpanStyle(
+                                color = GoogleRed,
+                                fontSize = 20.sp
+                            )
+                        ) {
+                            append("D")
+                        }
+
+                        withStyle(
+                            style = SpanStyle(
+                                color = GoogleGreen,
+                                fontSize = 20.sp
+                            )
+                        ) {
+                            append("J")
+                        }
+
+                        withStyle(
+                            style = SpanStyle(
+                                color = colorResource(id = R.color.JoinDiscordColor),
+                                fontSize = 20.sp
+                            )
+                        ) {
+                            append("'2")
+                        }
+                        withStyle(
+                            style = SpanStyle(
+                                color = GoogleYellow,
+                                fontSize = 20.sp
+                            )
+                        ) {
+                            append("2")
+                        }
+                    },
+                    fontWeight = FontWeight.Bold
+                )
+
+                Row {
+                    Text(
+                        text = "Discord",
+                        fontWeight = FontWeight.Bold,
+                        color = colorResource(id = R.color.JoinDiscordColor),
+                        fontSize = 20.sp
+                    )
+
+                    Image(
+                        painter = painterResource(id = R.drawable.arrow),
+                        contentDescription = null,
+                        modifier = Modifier.padding(top = 6.dp, start = 8.dp)
+                    )
+
+                }
+
+            }
+
+        }
+
+        Card(
+            backgroundColor = GreyBackground,
+            elevation = 0.dp,
+
+            ) {
+            Image(
+                painter = painterResource(id = R.drawable.hashtag), contentDescription = null,
+                modifier = Modifier.padding(start = 12.dp)
+            )
+
+            Column(modifier = Modifier.padding(top = 40.dp, start = 45.dp)) {
+                Text(
+                    text = "Hashtag",
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 30.sp
+                )
+
+                Text(
+                    text = "Trending Posts",
+                    color = Color.White,
+                    fontWeight = FontWeight.Normal,
+                    fontSize = 10.sp
+                )
+
+            }
+
+        }
+
 
     }
 }
+
+
+
+
 
